983d52bfb6d5467276a99c98a17fcca53ee5f428
Suciu Sorin <ssuciu7@gmail.com>
2018-12-18 19:50:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+//Array to be filled with the output result
+var output = [];
+
+//String to be filled with the users code
+var code = "";
+
+
+
+//Check if user input is correct
+function run() {
+
+  // Catch users input and fill var response with it
+  var result = document.getElementById("exerciseAnswer").value
+  for (var i = 0; i < result.length; i++) {
+    code += result[i]
+  }
+
+  // console.log equals this function which fills var outpu
+
+  console.log = function log(code) {
+        output.push(code);
+  }
+  // Execut user input code
+    eval(code);
+
+}
+
+// // Check if the response array is equal to the exercises array
+//   // for (var i = 0; i < exercises.length; i++) {
+//   //   for (var i = 0; i < response.length; i++){
+//   //     if (response[i] === exercises[i]) {
+//   //       document.getElementById("correct").innerHTML = "Your code is correct";
+//   //     } else {
+//   //       document.getElementById("incorrect").innerHTML = "Your code is incorrect.Pay attention to the rules";
+//   //     }
+//   //   }
+//   // }
